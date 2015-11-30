d2043c86010c4497de760cb7b09fcb8b1b4bad3e
DANIEL OREILLY <danieloreilly@DANIELs-MBP.fios-router.home>
2015-11-30 18:36:47 -0500

    player.swift introduced

---
+++ rpgoop/Player.swift
@@ -0,0 +1,32 @@
+//
+//  Player.swift
+//  rpgoop
+//
+//  Created by DANIEL OREILLY on 11/30/15.
+//  Copyright © 2015 DANIEL OREILLY. All rights reserved.
+//
+
+import Foundation
+
+
+class Player: Character {
+    private var _name = "Player"
+    
+    var name: String {
+        get {
+            return _name
+        }
+        
+    }
+    
+    private var _inventory = [String]()
+    
+    var inventory: [String] {
+        return _inventory
+    }
+    
+    convenience init(name:  String, hp: Int, attackPwr: Int) {
+        self.init(startingHp:  hp, attackPwr: attackPwr)
+        _name = name
+    }
+}

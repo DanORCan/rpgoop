6a29faf48298674868c88129a088557c04284d7e
DANIEL OREILLY <danieloreilly@DANIELs-MBP.fios-router.home>
2015-12-01 20:09:50 -0500

    Main Story Board ... first construction

--- rpgoop/ViewController.swift
+++ rpgoop/ViewController.swift
@@ -10,10 +10,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var printLbl: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+    
     }
 
 
